df3b5d6466c90fef2707948773eacced31021f88
Ratean <es_hay_em@list.ru>
2022-08-01 18:55:09 +0400

    Вызов сервиса в main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,28 +2,26 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-import java.sql.Connection;
-import java.sql.ResultSet;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
-        dao.createUsersTable();
-        dao.saveUser("Vanya", "Ivanov", (byte) 20);
-        dao.saveUser("Vanya", "Ivanov", (byte) 20);
-        dao.saveUser("Vanya", "Ivanov", (byte) 20);
-        dao.saveUser("Vanya", "Ivanov", (byte) 20);
-        List<User> list = dao.getAllUsers();
+        UserServiceImpl usa = new UserServiceImpl();
+        usa.createUsersTable();
+        usa.saveUser("Vanya", "Ivanov", (byte) 20);
+        usa.saveUser("Vanya", "Ivanov", (byte) 20);
+        usa.saveUser("Vanya", "Ivanov", (byte) 20);
+        usa.saveUser("Vanya", "Ivanov", (byte) 20);
+        List<User> list = usa.getAllUsers();
         System.out.println(list.size());
         for (User u: list) {
             System.out.println(u);
         }
-        dao.cleanUsersTable();
-        dao.dropUsersTable();
+        usa.cleanUsersTable();
+        usa.dropUsersTable();
 
     }
 }
